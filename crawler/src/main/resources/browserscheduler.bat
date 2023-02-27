eb0a56e69180cf828b1e835dc42cc7915930c1d7
Kimberly <mrkid863@gmail.com>
2023-02-27 20:02:09 +0800

    bump dependency and remove opentelemetry parameters from scripts

--- crawler/src/main/resources/browserscheduler.bat
+++ crawler/src/main/resources/browserscheduler.bat
@@ -1,2 +1,2 @@
 cd %~dp0
-start "" javaw -javaagent:opentelemetry-javaagent.jar -Dspring.profiles.active=jsonlog -Dotel.exporter.otlp.endpoint=${otlp_endpoint:-http://localhost:4317} -Dotel.resource.attributes=service.name=${project.artifactId},service.version=${project.version} -Dchrome_sandbox=no -Dwebdriver_use_default_user_data_dir=yes -cp "lib/;lib/*;ext/;ext/*" org.kquiet.browserscheduler.Launcher
+start "" javaw -Dspring.profiles.active=jsonlog -Dchrome_sandbox=no -Dwebdriver_use_default_user_data_dir=yes -cp "lib/;lib/*;ext/;ext/*" org.kquiet.browserscheduler.Launcher
